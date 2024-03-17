9bdc7510c85c4edf9b2c8f6b2a7209e8469153c5
Andris Buscariolli <andrisbuscariolli88@gmail.com>
2024-03-17 10:49:46 -0300

    test: ensure singUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/singUp.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './singUp'
+
+describe('SingUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: '',
+        password: '',
+        validPassword: ''
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
